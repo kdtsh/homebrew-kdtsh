5c65e17099404d1d91e6325b7561ce6e4d17dc33
Kenan Toker <32706804+kdtsh@users.noreply.github.com>
2022-12-10 14:59:30 +1100

    nicotine-plus upgraded to 3.2.7

--- Casks/nicotine-plus.rb
+++ Casks/nicotine-plus.rb
@@ -1,6 +1,6 @@
 cask "nicotine-plus" do
-  version "3.2.6"
-  sha256 "41983ffa1f42afdaf5534c65b06d921ed91861ceaad88aa0166837ae4985b79e"
+  version "3.2.7"
+  sha256 "d8dc38a8fc9dbab04cbde613c58653d863969a0ccf14e5d731d20f0326ea58a4"
 
   url "https://github.com/nicotine-plus/nicotine-plus/releases/download/#{version}/macos-installer.zip",
       verified: "github.com/nicotine-plus/nicotine-plus/"

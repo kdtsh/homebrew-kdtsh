e1d525560c59a572aafb5428820d8da56509e8f7
Kenan Toker <32706804+kdtsh@users.noreply.github.com>
2024-02-02 23:57:38 +1100

    fixed nicotine-plus download link

--- Casks/nicotine-plus.rb
+++ Casks/nicotine-plus.rb
@@ -2,7 +2,7 @@ cask "nicotine-plus" do
   version "3.3.0"
   sha256 "1dbb8e3b690e8263b9e9e5469ad79cd5d6cabff77912e1bd41ad09252163465e"
 
-  url "https://github.com/nicotine-plus/nicotine-plus/releases/download/#{version}/macos--arm64-installer.zip",
+  url "https://github.com/nicotine-plus/nicotine-plus/releases/download/#{version}/macos-arm64-installer.zip",
       verified: "github.com/nicotine-plus/nicotine-plus/"
   name "Nicotine+"
   desc "Cask for graphical client for the Soulseek file sharing network"

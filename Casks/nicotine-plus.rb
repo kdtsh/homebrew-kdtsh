ccffa9ac2cd058954ed6cf5b3d0ff6fd6c336906
Kenan Toker <32706804+kdtsh@users.noreply.github.com>
2026-09-16 14:53:38 +1000

    Remove deprecated `verified` parameter

--- Casks/nicotine-plus.rb
+++ Casks/nicotine-plus.rb
@@ -2,8 +2,7 @@ cask "nicotine-plus" do
   version "3.3.10"
   sha256 "ae752794c8b8683ed47ce3c45e1cd55730179c04ca214dfe625a404f5af37b45"
 
-  url "https://github.com/nicotine-plus/nicotine-plus/releases/download/#{version}/macos-arm64-installer.zip",
-      verified: "github.com/nicotine-plus/nicotine-plus/"
+  url "https://github.com/nicotine-plus/nicotine-plus/releases/download/#{version}/macos-arm64-installer.zip"
   name "Nicotine+"
   desc "Cask for graphical client for the Soulseek file sharing network"
   homepage "https://nicotine-plus.org/"
--- Casks/qbittorrent.rb
+++ Casks/qbittorrent.rb
@@ -2,8 +2,7 @@ cask "qbittorrent" do
   version "5.2.3"
   sha256 "9e37f6c7ff848c7bdd3c10167614c0cb78c00e2ddcc323f1ad3ac6c008a0481f"
 
-  url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}.dmg",
-      verified: "downloads.sourceforge.net/qbittorrent/qbittorrent-mac/"
+  url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}.dmg"
   name "qBittorrent"
   desc "Peer to peer Bitorrent client"
   homepage "https://www.qbittorrent.org/"
